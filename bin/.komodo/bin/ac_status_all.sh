b651fd1db6a6b3f48facfdb2abdaefe8549bfee0
ns408 <nsrea408@gmail.com>
2018-04-25 04:22:18 +1000

    Assetchain status script

---
+++ bin/.komodo/bin/ac_status_all.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e -m
+
+export KOMODO_CLI="<KOMODO_SRC_DIR>/src/komodo-cli"
+
+# start jobs in parallel
+for (( i=<AC_START>; i<=<AC_END>; i++ )); do
+
+  count=0
+  while [[ count -lt 120 ]]; do
+    if $($KOMODO_CLI -ac_name=$i getinfo >& /dev/null); then
+      getinfo=$($KOMODO_CLI -ac_name=$i getinfo)
+      if [[ $(echo $getinfo | jq -r .longestchain) -eq $(echo $getinfo | jq -r .blocks) ]]; then
+        echo -e "## ac_name=TXSCL$i in sync with the network ##"
+        break
+      fi
+    fi
+    count=${count}+1
+    sleep 1
+  done &
+
+done
+
+# Wait for all parallel jobs to finish
+while [ 1 ]; do fg 2> /dev/null; [ $? == 1 ] && break; done
