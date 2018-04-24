e924198008a45a1ccd88831819e29fc45a0e752e
ns408 <nsrea408@gmail.com>
2018-04-25 04:22:36 +1000

    Assetchain stop script

---
+++ bin/.komodo/bin/ac_stop_all.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e -m
+
+export KOMODO_CLI="<KOMODO_SRC_DIR>/src/komodo-cli"
+
+# start jobs in parallel
+for (( i=<AC_START>; i<=<AC_END>; i++ )); do
+
+  count=0
+  while [[ count -lt 120 ]]; do
+    if $($KOMODO_CLI -ac_name=$i getinfo >& /dev/null); then
+      echo -e "## Stopping ac_name=TXSCL$i ##"
+      $KOMODO_CLI -ac_name=$i stop
+      break
+    fi
+    count=${count}+1
+    sleep 1
+  done &
+
+done
+
+# Wait for all parallel jobs to finish
+while [ 1 ]; do fg 2> /dev/null; [ $? == 1 ] && break; done
