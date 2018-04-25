c8b4f70174a3828b0bedd9773f9093f31918e45d
ns408 <nsrea408@gmail.com>
2018-04-25 14:09:46 +1000

    Convert to function and see if we can escape printing the whole damn code while bringing things up in the foreground.

--- bin/.komodo/bin/ac_stop_all.sh
+++ bin/.komodo/bin/ac_stop_all.sh
@@ -1,24 +1,49 @@
 #!/usr/bin/env bash
 set -e
 
+# Enable Job Control
+set -m
+
 export KOMODO_CLI="<KOMODO_SRC_DIR>/src/komodo-cli"
 
-set -m
+function komodod_stop () {
+  while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+      -seq)
+        export komodod_run_SEQUENCE="${2}"
+        shift
+      ;;
+      *)
+        echo "WARNING: Unknown option $key" >&2
+        exit 1
+      ;;
+    esac
+    shift
+  done
+
+  # stop the assetchain in 120 seconds or fail
+  count=0
+  while [[ count -lt 120 ]]; do
+    if ! $(ps aux | grep -w "ac_name=TXSCL${komodod_run_SEQUENCE}" | grep -v grep) >& /dev/null; then
+      if $($KOMODO_CLI -ac_name=TXSCL${komodod_run_SEQUENCE} getinfo >& /dev/null); then
+        echo -e "## Stopping ac_name=TXSCL${komodod_run_SEQUENCE} ##"
+        $KOMODO_CLI -ac_name=TXSCL${komodod_run_SEQUENCE} stop
+        break
+      fi
+    fi
+    count=${count}+1
+    sleep 1
+  done
+}
+
+komodod_stop &
 
 # start jobs in parallel
 for (( i=<AC_START>; i<=<AC_END>; i++ )); do
   mod_i=$(printf "%03d " $i)
-  count=0
-  while [[ count -lt 120 ]]; do
-    if $($KOMODO_CLI -ac_name=TXSCL${mod_i} getinfo >& /dev/null); then
-      echo -e "## Stopping ac_name=TXSCL${mod_i} ##"
-      $KOMODO_CLI -ac_name=TXSCL${mod_i} stop
-      break
-    fi
-    count=${count}+1
-    sleep 1
-  done &
 
+  komodod_stop -seq $mod_i  &
 done
 
 # Wait for all parallel jobs to finish
