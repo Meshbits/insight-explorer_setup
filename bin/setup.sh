f0ac014d5376cb1a6813a1b915ce5b4795561ed0
ns408 <nsrea408@gmail.com>
2018-04-25 17:15:01 +1000

    Nodejs is only needed for insight so moving it out of the setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -134,11 +134,6 @@ apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold
   install \
   git sudo jq dnsutils wget tree inotify-tools htop
 
-# Install nodejs
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-# Install mongodb
 
 userdatasetup_log="/home/${SCRIPTUSER}/userdatasetup_log"
 # Setup komodod
