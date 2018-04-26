32671ef0dfd89f8dae474f2ef17b91834224bceb
ns408 <nsrea408@gmail.com>
2018-04-26 18:20:46 +1000

    Renaming the script and changing the logic to make things a bit more safe.

---
+++ bin/.komodo/bin/careful_ac_purge_all.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -e
+
+read -p "## Warning: You are about to purge all your assetchains listed in coinlist file. \
+\nPlease enter [y]es or [y] \n ##" userinput
+
+case ${userinput} in
+  ( YES | yes | Yes | Y | y )
+    echo -e '## Purging assetchain folders ##'
+  ;;
+  (*)
+    echo -e "You didn't enter a valid option, existing.."
+    exit 1
+  ;;
+esac
+
+# source profile
+source /etc/profile
+[[ -f ${HOME}/.common/config ]] && source ${HOME}/.common/config
+
+# Get the coinlist; store name and value in variables
+if [[ -f ${HOME}/.common/coinlist ]]; then
+  source ${HOME}/.common/coinlist
+else
+  echo -e "Coinlist not found. Exiting.."
+  exit 1
+fi
+
+# start jobs in parallel
+for item in "${coinlist[@]}"; do
+  coin_name=$(echo "${item}" | cut -d' ' -f1)
+
+  rm -f ${HOME}/${coin_name}
+  rm -rf ${HOME}/.komodo/${coin_name}
+done
+
+# Wait for all parallel jobs to finish
+while [ 1 ]; do fg >& /dev/null; [ $? == 1 ] && break; done
